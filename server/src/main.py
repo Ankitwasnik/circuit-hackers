85d8769b8208ee2f36db12ce36c1c6b12ed29f39
Amit Singh <one.amitsingh@gmail.com>
2025-02-22 18:19:48 +0530

    port bug fix

--- server/src/main.py
+++ server/src/main.py
@@ -37,5 +37,5 @@ async def query_batting():
 if __name__ == "__main__":
     import uvicorn
 
-    port = os.getenv("API_PORT", 8000)
+    port = int(os.getenv("API_PORT", "8000"))
     uvicorn.run(app, host="0.0.0.0", port=port)
